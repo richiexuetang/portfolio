d4bc867cb562fe9ee1634e792ef717c6290230d7
Richard Tang <rxtang@ucsd.edu>
2021-03-11 18:51:18 -0800

    added bootstrap and base template

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,7 +21,7 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home, name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
